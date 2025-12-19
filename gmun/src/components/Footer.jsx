3174c85394f24281aea44dd4771f83591e04acc3
Tejal Saurabhi <152639200+TejalSaurabhi@users.noreply.github.com>
2025-12-19 10:46:19 +0530

    Fix path for Committees link in Footer

--- gmun/src/components/Footer.jsx
+++ gmun/src/components/Footer.jsx
@@ -6,7 +6,7 @@ import cqLogo from "../images/CQ_Logo_White.webp";
 const QUICK_LINKS = [
   { name: "Home", path: "/" },
   { name: "About Us", path: "/AboutUs" },
-  { name: "Committees", path: "/committees/1" },
+  { name: "Committees", path: "/committee/1" },
   { name: "FAQs", path: "/faqs" },
   { name: "Gallery", path: "/gallery" },
 ];
